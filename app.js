517d10b6775c68cf9e381f48aa60d6fbe93f6ee4
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-16 20:35:40 +0300

    project 1

--- app.js
+++ app.js
@@ -6,10 +6,42 @@ const creatTable =((rows,cols) => {
         const row = document.createElement('tr');
         for (j=0; j<cols; j++) {
         const cell = document.createElement('td')
-         row.appendChild(cell);
+        row.appendChild(cell);
         }
        table.appendChild(row)
     }
 })
+creatTable(6,5)
 
-console.log(creatTable(6,5))
+/*--------------------------------constant--------------------------------------------------*/
+
+const wordList = []
+const maxGuess = 6 
+
+/*--------------------------------variables --------------------------------------------------*/
+
+let rightWord
+let playerGuess = []
+let feedback 
+
+/*--------------------------------cached element refrence -----------------------------------*/
+
+const inputWord = document.getElementById('dataInput');
+const submit = document.getElementById('submitBtn');
+
+/*----------------------------------functions--------------------------------------------------*/
+
+submit.addEventListener('click' , () => {
+const inputData = inputWord.value.trim();
+if (inputData) {
+playerGuess.push(inputData);
+
+inputWord.value =''; 
+}
+})
+ 
+playerGuess.forEach((Guess) => {
+const listItem = document.createElement('li');
+listItem.textContent = dataItem;
+inputWord.appendChild(listItem);
+})
