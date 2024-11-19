e130d845f6a808cab7655e0cb468755e8984cb75
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-19 10:41:02 +0300

    project 1

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
 
-const creatTable =((rows,cols) => {
-    const table = document.getElementById('myTable')
-    for (let i=0 ; i< rows ; i++)
-    {
-        const row = document.createElement('tr');
-        for (j=0; j<cols; j++) {
-        const cell = document.createElement('td')
-        row.appendChild(cell);
-        }
-       table.appendChild(row)
-    }
-})
-creatTable(6,5)
-
 /*--------------------------------constant--------------------------------------------------*/
 
-const wordList = []
+const wordList = ['pizza', 'quick', 'jerky', 'crazy', 'cozey', 'jocks']
 const maxGuess = 6 
 
 /*--------------------------------variables --------------------------------------------------*/
-
-let rightWord
-let playerGuess = []
+let Guess = 0;
+let rightWord ="";
 let feedback 
-
+let letters
+let round = 0;
 /*--------------------------------cached element refrence -----------------------------------*/
 
-const inputWord = document.getElementById('dataInput');
-const submit = document.getElementById('submitBtn');
-
+const start = document.getElementById('start game');
+const inputWord = document.getElementById('guessInput');
+const submit = document.getElementById('submit');
+const restart = document.getElementById('restart');
+const next = document.getElementById('next');
+const message = document.getElementById('message')
+const table = document.getElementById('myTable')
 /*----------------------------------functions--------------------------------------------------*/
 
-submit.addEventListener('click' , () => {
-const inputData = inputWord.value.trim();
-if (inputData) {
-playerGuess.push(inputData);
+inputWord.style.display="none";
+submit.style.display="none";
+restart.style.display="none";
+next.style.display="none";
+message.style.display="none";
 
-inputWord.value =''; 
+// function to start the game 
+const startGame = () => {
+    
+    start.style.display="none";
+    inputWord.style.display="block";
+    submit.style.display="block";
+    message.style.display="block"
+
+    rightWord = wordList[round];
+    message.textContent = "Guess the 5-letters word"
 }
-})
- 
-playerGuess.forEach((Guess) => {
-const listItem = document.createElement('li');
-listItem.textContent = dataItem;
-inputWord.appendChild(listItem);
-})
+
+start.addEventListener("click" , startGame);
+
+//function to take data from input field and split it 
+const submitWord = () => {
+    const playerGuess = inputWord.value.toLowerCase();
+    letters = playerGuess.split("");
+    console.log(playerGuess)
+    console.log(letters)
+    console.log(rightWord)
+    inputWord.value="";
+//table.rows.length-1
+
+for (var i = 0; i < table.rows.length-1 ; i++)
+{
+    // let cell = table.rows[Guess].cells[i]
+    //let cell = document.getElementById(`col${i}row${Guess}`)
+    table.rows[Guess].cells[i].innerHTML = letters[i]  
+}   
+    //check the word 
+    /*
+    if ( playerGuess == rightWord ){
+      let cell = document.getElementById(`col${i}row${Guess}`)
+      message.textContent = "congratulations! you found the right word ;)"
+      cell.style.backgroundColor='green'
+      inputWord.style.display="none";
+      submit.style.display="none";
+      next.style.display="block";
+    }
+    */
+// check the player guess 
+  
+    //the cells of the word will turn to green 
+    
+   // nextround()
+}
+submit.addEventListener("click" , submitWord);
+/*
+const nextround = () => {
+    round++;
+    rightWord = wordList[round];
+    next.style.display="none";
+    inputWord.style.display="block";
+    submit.style.display="block";
+    message.style.display="block"
+    submitWord()
+}
+next.addEventListener("click" , nextround)
+}
+*/
+
