55e4dfeb2247b685b0fef133ce83ec8b2354aec2
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-19 16:22:19 +0300

    project 1

--- app.js
+++ app.js
@@ -43,13 +43,10 @@ const startGame = () => {
 
 start.addEventListener("click" , startGame);
 
-//function to take data from input field and return feedback 
+//function to take data from input field 
 const submitWord = () => {
     playerGuess = inputWord.value.toLowerCase();
     letters = playerGuess.split("");
-    console.log(playerGuess)
-    console.log(letters)
-    console.log(rightWord)
     inputWord.value="";
 
 for (var i = 0; i < table.rows.length-1 ; i++)
@@ -57,38 +54,47 @@ for (var i = 0; i < table.rows.length-1 ; i++)
     // let cell = table.rows[Guess].cells[i]
     cell = document.getElementById(`col${i}row${Guess}`)
     cell.innerHTML = `${letters[i]}` ;
-    checkWord()
-}
+}   
+checkWord()
 }
 
+//check word function 
 const checkWord = () => {
-
-    if ( playerGuess == rightWord ){
-        message.textContent = "congratulations! you found the right word ;)"
-        table.rows[Guess].style.backgroundColor='green'
+    if ( playerGuess === rightWord ){
+        for (let i = 0; i < table.rows.length-1 ; i++) {
+            cell = document.getElementById(`col${i}row${Guess}`)
+            cell.style.backgroundColor='green'
+        }
+        //table.rows[Guess].style.backgroundColor='green';
+        message.style.display="block"
+        message.textContent = "congratulations! you found the right word ;)";
         inputWord.style.display="none";
         submit.style.display="none";
         next.style.display="block";
-        nextRound()
-}
-
-}
-
-const nextRound = () => {
-    for (let row of table.rows) {
-        // Loop through all cells in the row
-        for (let cell of row.cells) {
-          // Clear the content of each cell
-          cell.innerHTML = "";
-          cell.style.backgroundColor = ""; 
         }
-      }
-    message.textContent = "Guess the 5-letters word"
-    round++;
-    rightWord = wordList[round];
-    
-}
-next.addEventListener("click" , nextRound);
+
+    if (playerGuess !== rightWord){
+        let commonLetters =[];
+        for (let i = 0; i < table.rows.length-1 ; i++) {
+        let letter1 = playerGuess[i];
+        let letter2 = rightWord[i];
+        cell = document.getElementById(`col${i}row${Guess}`)
+        // Check if the letter is the same in both words at the same position
+        if (letter1 === letter2) {    
+            cell.style.backgroundColor='green'
+        } else if (rightWord.includes(letter1)){
+            cell.style.backgroundColor='yellow'  
+        } else {
+            cell.style.backgroundColor='red'
+        }
+        }
+        message.textContent = "Nice try, guess again!";
+        }
+             
+    }
+
+//next round function
+
 
 
 
